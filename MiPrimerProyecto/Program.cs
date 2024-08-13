d35bdbab079bbf8ff80e0709f5f8602609421141
GarciaJosue21 <josueee.egl@gmail.com>
2024-08-13 16:42:44 -0600

    Update Program.cs
    Suma de dos numeros  pero con el tyipo sbyte

--- MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/Program.cs
@@ -26,3 +26,34 @@ namespace MiPrimerProyecto
         }
     }
 }
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Num1: ");
+            sbyte num1 = sbyte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Num2: ");
+            sbyte num2 = sbyte.Parse(Console.ReadLine());
+
+            int suma = num1 + num2;
+
+            Console.WriteLine("La suma de  {0} + {1} = {2}", num1, num2, suma);
+
+            //Pausa
+            Console.ReadLine();
+
+        }
+    }
+}
